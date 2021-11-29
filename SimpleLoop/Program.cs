eb5fd34c3cf283f1a85eb014efee611c1b08dfb0
Алексей Федюшин <alexeyfedyshinural@gmail.com>
2021-11-29 14:25:44 +0500

    Добавьте файлы проекта.

---
+++ SimpleLoop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SimpleLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Console.WriteLine("Введите фразу, которую необходимо повторить: ");
+            string userText = Console.ReadLine();
+
+            Console.WriteLine("Укажите количество повторений: ");
+            int totalRepetitions = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 0; i < totalRepetitions; ++i)
+            {
+                Console.WriteLine(userText);
+            }
+        }
+    }
+}
